0a6c2385c406addd822cb865a3cd5044c81b44a6
NayrbEroom <moorebryan21@gmail.com>
2019-10-02 18:32:46 -0500

    added base blob and added to index

--- Blob/Blob.js
+++ Blob/Blob.js
@@ -5,4 +5,14 @@
 function Blob(x, y, r) {
 
 
+  this.pos = createVector(x, y);
+
+
+
+  this.show = function() {
+
+    fill(255);
+    ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
+  }
+
 }
