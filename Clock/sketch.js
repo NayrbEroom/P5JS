fb586d3d2f9ac53c1463c1ba797a784d31703c45
NayrbEroom <moorebryan21@gmail.com>
2019-12-30 01:47:12 -0600

    new project dicking around with a clock

---
+++ Clock/sketch.js
@@ -0,0 +1,25 @@
+
+curWinWidth = windowWidth;
+curWinHeight = windowHeight;
+
+function setup() {
+
+
+    createCanvas(windowWidth, windowHeight);
+
+
+
+    console.log
+}
+
+function draw() {
+  background(51);
+
+  if (curWinWidth != windowWidth) {
+    if (curWinHeight != windowHeight) {
+      createCanvas(windowWidth, windowHeight);
+      }
+    }
+
+
+}
