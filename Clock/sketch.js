f523f294dee786ddf873893c5a1ff3ed8916f9ba
NayrbEroom <moorebryan21@gmail.com>
2019-12-30 14:32:09 -0600

    .

--- Clock/sketch.js
+++ Clock/sketch.js
@@ -1,32 +1,43 @@
 
-
+var d = day();
+var h = hour();
+var m = minute();
+var s = second();
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     console.log("Start success");
-    let d = day();
-    let h = hour();
-    let m = minute();
-    let s = second();
+
 
     console.log("I've received the time");
-    
+
 
 }
 
 function draw() {
   background(51);
-
+  stroke(0);
+  text(d + ':' + h + ':' + m + ':' + s, 10, 10);
   let midScreenX = windowWidth/2;
   let midScreenY = windowHeight/2;
+
+  noFill();
+  stroke(255, 102, 204);
+  strokeWeight(10);
   let dayArc = arc(midScreenX, midScreenY, 50, 50, 0, 0);
-  nofill();
+  noFill();
+  stroke(102, 255, 51);
+  strokeWeight(10);
   let hourArc = arc(midScreenX, midScreenY, 100, 100, 0, 0);
-  nofill();
+  noFill();
+  stroke(102, 102, 255);
+  strokeWeight(10);
   let minuteArc = arc(midScreenX, midScreenY, 150, 150, 0, 0);
-  nofill();
+  noFill();
+  stroke(255, 80, 80);
+  strokeWeight(10);
   let secondArc = arc(midScreenX, midScreenY, 200, 200, 0, 0);
-  nofill();
+
 
 
 }
