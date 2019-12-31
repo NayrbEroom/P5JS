982567585b46b4c64d19e9d4bb29276948f11125
NayrbEroom <moorebryan21@gmail.com>
2019-12-31 08:29:43 -0600

    fix commit, had to get rid of .git/index.lock

---
+++ Default/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    console.log("Start success");
+
+
+}
+
+function draw() {
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  console.log("Resized");
+}
